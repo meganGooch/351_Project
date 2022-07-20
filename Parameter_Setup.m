1e8ec403aed22b47da8770ffb78248d9760c354c
JPCasasanta <39733244+JPCasasanta@users.noreply.github.com>
2022-07-20 17:14:06 -0400

    Add files via upload

---
+++ Parameter_Setup.m
@@ -0,0 +1,48 @@
+%SYDE 351 Final Project
+%Group 27
+%Task 0-B
+%Paramater Calculations and Setup
+
+%File is used to define relevant information to the project externally
+%This includes givens, such as the mass
+%This includes calculations, such as the moment of inertia
+
+%List givens
+
+m_load = 20;                    %mass of the load carried [kg]
+m_robot = 20;                   %mass of the robot itself [kg]
+
+m_total = m_load + m_robot;     %mass of the system
+
+H_robot = 1;                    %height of the robot [m]
+
+D_robot = 0.5;                  %diameter of the robot [m]
+R_robot = D_robot/2;            %radius of the robot [m]
+
+
+D_wheel = 0.1;                  %diameter of the robot wheels [m]
+R_wheel = D_wheel/2;            %radius of the robot wheels [m]
+
+%Assumed Dimensions/Geometry
+
+theta_wheels = (pi / 4);
+d_COM = R_robot * cos(theta_wheels);
+L_wheel = R_robot * sin(theta_wheels);
+
+
+
+
+%Assumption: the robot can be modelled with uniform mass
+
+J_robot = 0.5 * m_total * ((R_robot)^2); 
+                                %moment of inertia about the cyllinder axis 
+                                %passing through both central circles
+J_total = J_robot + (m_total * (d_COM^2));
+
+%Motor Characteristics
+
+N = 1;
+K = 1;
+Ra = 1;
+La = 0.001;
+
