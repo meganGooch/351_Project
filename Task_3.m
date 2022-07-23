c5b7b0ac684dcf1daf8c5c38ff972e71bdfa0cbf
shahedSaleh <109485973+shahedSaleh@users.noreply.github.com>
2022-07-23 16:05:15 -0400

    Add files via upload

--- Task_3.m
+++ Task_3.m
@@ -1,9 +1,9 @@
-%SYDE 351 Final Projct 
+%SYDE 351 Final Project
 %Group 27
 %Task 3
 
 clear;
-clc; 
+clc;
 
 %floor dimensions given in m
 tile_width = 0.3048;
@@ -15,21 +15,20 @@ floor_length = 5;
 duty_cycle = tile_width/(grout_width + tile_width)*100;
 period = tile_width + grout_width;
 
-%graph of vertical displacment in terms of horizontal displacement 
+%graph of vertical displacement in terms of horizontal displacement
 x = 0:0.001:floor_length;
 y = grout_depth/2*square(2*pi*(1/period)*x, duty_cycle)-grout_depth/2;
 
-%plot the graph 
+%plot the graph
 plot (x,y);
 grid on;
 title('Tile Floor Displacement Signal');
-xlabel('Horzontal Displacement [m]');
+xlabel('Horizontal Displacement [m]');
 ylabel('Vertical Displacement [m]');
 
-%test the vertical displacment in the vibration simulation 
+%test the vertical displacement in the vibration simulation
 t = x;
 [t_simulation, y_simulation, d_simulation, x_simulation, result] = run_vibration_simulation(t, y);
 
 %check if the drink spills
 disp(result);
-
--- Task_4.m
+++ Task_4.m
@@ -1,9 +1,9 @@
-%SYDE 351 Final Projct 
+%SYDE 351 Final Project
 %Group 27
 %Task 4
 
 clear;
-clc; 
+clc;
 
 %floor dimensions given in m
 threshold_width = 0.05;
@@ -17,21 +17,21 @@ y = zeros(size(x));
 
 %generate graph
 for i = 1:size(y, 2)
-    if ((start_distance <= x(i)) && (x(i) <= start_distance + threshold_width))
-        y(i) = (x(i) - start_distance) * 2 * pi / threshold_width;
-        y(i) = (1 - cos(y(i))) * 0.5 * threshold_height;
-    else
-        y(i) = 0;    
-    end
+   if ((start_distance <= x(i)) && (x(i) <= start_distance + threshold_width))
+       y(i) = (x(i) - start_distance) * 2 * pi / threshold_width;
+       y(i) = (1 - cos(y(i))) * 0.5 * threshold_height;
+   else
+       y(i) = 0;   
+   end
 end
 
 %plot the graph
 plot(x, y);
 title('Door Threshold Displacement Signal');
-xlabel('Horzontal Displacement [m]');
+xlabel('Horizontal Displacement [m]');
 ylabel('Vertical Displacement [m]');
 
-%test the vertical displacment in the vibration simulation 
+%test the vertical displacement in the vibration simulation
 t = x;
 [t_simulation, y_simulation, d_simulation, x_simulation, result] = run_vibration_simulation(t, y);
 
